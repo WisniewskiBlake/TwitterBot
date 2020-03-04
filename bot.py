78728fdb56ac47425e1f7be5d0df96c94fc53989
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-03-04 04:22:56 -0500

    grabbed text from first tweet mention

--- bot.py
+++ bot.py
@@ -5,4 +5,7 @@ auth = tweepy.OAuthHandler(
 'tLCxAAJ4LeITmiKS5uJPqU6yUq3HiBBzYt4wU42HGh4WOl6AXQ')
 auth.set_access_token('1235093465098317824-vynQuEZSa63PTdd8cBs2L6oB0JI4oz' , 'yu3HrQwOwjfJeRMMCzXRpaPmrubr5LXQW15XhW9X4Krpc')
 api = tweepy.API(auth)
-api.update_status('tweepy + oauth!')
+
+
+tweets = api.mentions_timeline()
+print(tweets[1].text)
