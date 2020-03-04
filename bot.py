e9b62a62b6636295ea7597bcb3fdfcfbd5aa356f
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-03-04 04:54:36 -0500

    made changes to api formate

--- bot.py
+++ bot.py
@@ -1,11 +1,13 @@
 import tweepy
 
-auth = tweepy.OAuthHandler(
-'O7r4jS9lM1TlTo1cZq0WWUyVt', 
-'tLCxAAJ4LeITmiKS5uJPqU6yUq3HiBBzYt4wU42HGh4WOl6AXQ')
-auth.set_access_token('1235093465098317824-vynQuEZSa63PTdd8cBs2L6oB0JI4oz' , 'yu3HrQwOwjfJeRMMCzXRpaPmrubr5LXQW15XhW9X4Krpc')
-api = tweepy.API(auth)
+consumer_key = 'O7r4jS9lM1TlTo1cZq0WWUyVt'
+consumer_secret = 'tLCxAAJ4LeITmiKS5uJPqU6yUq3HiBBzYt4wU42HGh4WOl6AXQ'
+key = '1235093465098317824-vynQuEZSa63PTdd8cBs2L6oB0JI4oz'
+secret = 'yu3HrQwOwjfJeRMMCzXRpaPmrubr5LXQW15XhW9X4Krpc'
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+api = tweepy.API(auth)
 
 tweets = api.mentions_timeline()
 print(tweets[1].text)
