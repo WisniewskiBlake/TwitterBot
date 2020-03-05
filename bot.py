3c2a7512db6df7dd3ddb46b8887379ccd5c07ad6
BlakeWisniewski <wisniewski.blake@gmail.com>
2020-03-05 01:37:56 -0500

    storing id of last tweet properly

--- bot.py
+++ bot.py
@@ -9,10 +9,23 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 api = tweepy.API(auth)
 
-tweets = api.mentions_timeline()
+FILE_NAME = 'last_seen.txt'
 
+def read_last_seen(FILE_NAME):
+    file_read = open(FILE_NAME, 'r')
+    last_seen_id = int(file_read.read().strip())
+    file_read.close()
+    return last_seen_id
 
+def store_last_seen(FILE_NAME, last_seen_id):
+    file_write = open(FILE_NAME, 'w')
+    file_write.write(str(last_seen_id))
+    file_write.close()
+    return
 
+tweets = api.mentions_timeline(read_last_seen(FILE_NAME), tweet_mode='extended')
 for tweet in tweets:
-    if '#randomtweet' in tweet.text.lower():
-        print(str(tweet.id) + ' - ' + tweet.text)
+    if '#randomtweet' in tweet.full_text.lower():
+        print(str(tweet.id) + ' - ' + tweet.full_text)
+
+        store_last_seen(FILE_NAME, tweet.id)
